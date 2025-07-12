402185d2ba17aaa9a849962c6a7071dd9f87f408
wangyinhe <wyinhe@sina.cn>
2025-07-12 12:14:43 +0800

    批量执行测试用例调整

--- gop/runner_batch_test.go
+++ gop/runner_batch_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/simonwater/gopression/chk"
 	"github.com/simonwater/gopression/gop"
 	"github.com/simonwater/gopression/gop/testdata"
 	fileutil "github.com/simonwater/gopression/util/files"
@@ -18,7 +19,7 @@ const (
 
 // 测试解析执行
 func TestBatchRunner_IR(t *testing.T) {
-	fmt.Printf("批量运算测试(解析执行)")
+	fmt.Println("===批量运算测试(解析语法树执行)")
 	lines := testdata.GetExpressions(gop_formulaBatches)
 	runner := gop.NewGopRunner()
 	runner.SetExecuteMode(gop.SyntaxTree)
@@ -34,7 +35,7 @@ func TestBatchRunner_IR(t *testing.T) {
 
 // 测试编译+字节码执行
 func TestBatchRunner_CompileChunk(t *testing.T) {
-	fmt.Printf("批量运算测试(编译+字节码执行)")
+	fmt.Println("===批量运算测试(编译+字节码执行)")
 	start := time.Now()
 
 	lines := testdata.GetExpressions(gop_formulaBatches)
@@ -52,30 +53,45 @@ func TestBatchRunner_CompileChunk(t *testing.T) {
 	elapsed := time.Since(start)
 	fmt.Printf("总耗时: %s", elapsed)
 	fmt.Printf("==========\n")
-
-	// 序列化字节码
-	fileName := "Chunks.pb"
-	filePath := fileutil.GetTestPath(gop_testDirectory, fileName)
-	err = writeChkFile(chunk, filePath)
-	require.NoError(t, err, "序列化字节码失败")
 }
 
+// 测试编译到文件再从文件读取执行
 func TestBatchRunner_MultiChunks(t *testing.T) {
-	fmt.Printf("字节码直接执行\n")
-	start := time.Now()
-	// 反序列化字节码
-	fileName := "Chunks.pb"
-	filePath := fileutil.GetTestPath(gop_testDirectory, fileName)
-	chunk, err := readChkFile(filePath)
-	require.NoError(t, err, "反序列化字节码失败")
+	fmt.Println("===字节码编译到文件再从文件读取执行")
+	filePath := fileutil.GetTestPath(gop_testDirectory, "Chunks.pb")
+	chunk, err := createAndGetChunk(t, filePath)
+	require.NoError(t, err, "字节码创建失败")
+	fmt.Printf("字节码大小%vKB\n", chunk.GetByteSize()/1024)
 
-	for i := 0; i < 1; i++ {
+	start, cnt := time.Now(), 1
+	for i := 0; i < cnt; i++ {
 		runner := gop.NewGopRunner()
 		env := testdata.GetEnv(gop_formulaBatches)
 		_ = runner.RunChunk(chunk, env)
 		require.NoError(t, err, "执行字节码失败")
 		testdata.CheckValues(t, env, gop_formulaBatches)
 	}
-	fmt.Printf("完成。耗时: %s\n", time.Since(start))
+	fmt.Printf("执行完成。执行次数：%v。耗时: %s\n", cnt, time.Since(start))
 	fmt.Printf("==========\n")
+}
+
+func createAndGetChunk(t *testing.T, filePath string) (*chk.Chunk, error) {
+	lines := testdata.GetExpressions(gop_formulaBatches)
+	runner := gop.NewGopRunner()
+	start := time.Now()
+	chunk, err := runner.CompileSource(lines)
+	require.NoError(t, err, "编译失败")
+	fmt.Printf("编译完成。耗时: %s\n", time.Since(start))
+
+	// 序列化字节码
+	start = time.Now()
+	err = writeChkFile(chunk, filePath)
+	require.NoError(t, err, "序列化字节码失败")
+	fmt.Printf("序列化到文件完成。耗时: %s\n", time.Since(start))
+
+	// 反序列化
+	start = time.Now()
+	chunk, err = readChkFile(filePath)
+	fmt.Printf("从文件反序列化完成。耗时: %s\n", time.Since(start))
+	return chunk, err
 }
--- gop/serialize_test.go
+++ gop/serialize_test.go
@@ -7,8 +7,10 @@ import (
 	"time"
 
 	"github.com/simonwater/gopression/chk"
+	"github.com/simonwater/gopression/env"
 	"github.com/simonwater/gopression/gop"
 	"github.com/simonwater/gopression/gop/testdata"
+	"github.com/simonwater/gopression/ir"
 	fileutil "github.com/simonwater/gopression/util/files"
 	"github.com/stretchr/testify/require"
 )
@@ -19,7 +21,7 @@ const (
 )
 
 func TestChunkSerialization(t *testing.T) {
-	fmt.Printf("序列化反序列化测试：\n")
+	fmt.Printf("===序列化反序列化测试：\n")
 	chunkSerializeTest(t)
 }
 
@@ -27,36 +29,36 @@ func chunkSerializeTest(t *testing.T) {
 	// 创建表达式列表
 	lines := testdata.GetExpressions(ser_formulaBatches)
 	fmt.Printf("表达式总数：%d\n", len(lines))
-
 	runner := gop.NewGopRunner()
-
 	// 解析和分析表达式
 	fmt.Printf("开始解析和分析：\n")
 	start := time.Now()
 	exprs, err := runner.Parse(lines)
 	require.NoError(t, err, "解析失败")
-
 	exprInfos := runner.Analyze(exprs)
 	require.NoError(t, err, "分析失败")
-
 	elapsed := time.Since(start)
 	fmt.Printf("中间结果生成完成。耗时: %s\n", elapsed)
-
 	// 编译为字节码
-	runner.SetTrace(true)
 	chunk := runner.CompileIR(exprInfos)
+	env := testdata.GetEnv(ser_formulaBatches)
 
+	chunkTest(t, chunk, env)
+	SyntaxTreeTest(t, exprInfos, env)
+
+	fmt.Printf("==========\n")
+}
+
+func chunkTest(t *testing.T, chunk *chk.Chunk, ev env.Environment) {
 	// 序列化字节码
 	chunkSize := chunk.GetByteSize()
-	fmt.Printf("开始进行字节码序列化反序列化，字节码大小(KB): %d\n", chunkSize/1024)
-
-	start = time.Now()
+	fmt.Printf("==开始进行字节码序列化反序列化，字节码大小(KB): %d\n", chunkSize/1024)
+	start := time.Now()
 	fileName := "Chunks.pb"
 	filePath := fileutil.GetTestPath(ser_testDirectory, fileName)
-
-	err = writeChkFile(chunk, filePath)
+	err := writeChkFile(chunk, filePath)
 	require.NoError(t, err, "序列化失败")
-	elapsed = time.Since(start)
+	elapsed := time.Since(start)
 	fmt.Printf("字节码已序列化到文件：%s 耗时: %s\n", fileName, elapsed)
 
 	// 反序列化字节码
@@ -69,26 +71,27 @@ func chunkSerializeTest(t *testing.T) {
 	// 执行反序列化的字节码
 	fmt.Printf("开始执行字节码：\n")
 	start = time.Now()
-	env := testdata.GetEnv(ser_formulaBatches)
+	runner := gop.NewGopRunner()
+	_ = runner.RunChunk(deserializedChunk, ev)
 
-	_ = runner.RunChunk(deserializedChunk, env)
-
-	testdata.CheckValues(t, env, ser_formulaBatches)
+	testdata.CheckValues(t, ev, ser_formulaBatches)
 	elapsed = time.Since(start)
 	fmt.Printf("字节码执行完成。耗时: %s\n", elapsed)
+}
 
-	// 执行语法树（IR）作为对比
-	fmt.Printf("开始执行语法树\n")
-	start = time.Now()
-	env = testdata.GetEnv(ser_formulaBatches)
+func SyntaxTreeTest(t *testing.T, exprInfos []*ir.ExprInfo, ev env.Environment) {
+	// todo 序列化语法树
+	// todo反序列化语法树
 
-	_ = runner.RunIR(exprInfos, env)
+	// 执行语法树
+	fmt.Printf("==开始执行语法树\n")
+	start := time.Now()
+	runner := gop.NewGopRunner()
+	_ = runner.RunIR(exprInfos, ev)
 
-	testdata.CheckValues(t, env, ser_formulaBatches)
-	elapsed = time.Since(start)
+	testdata.CheckValues(t, ev, ser_formulaBatches)
+	elapsed := time.Since(start)
 	fmt.Printf("语法树执行完成。耗时: %s\n", elapsed)
-
-	fmt.Printf("==========\n")
 }
 
 func writeChkFile(chunk *chk.Chunk, filePath string) error {
